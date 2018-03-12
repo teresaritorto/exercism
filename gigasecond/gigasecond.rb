442aec0837533b94c969bcb4c2af00b12dccc9a2
Teresa Ritorto <tritorto@gmail.com>
2018-03-12 17:16:15 +1100

    added guard statement

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -4,6 +4,7 @@ class Gigasecond
     GIGASECOND = 10**9
     
     def self.from start_date
+        raise ArgumentError unless start_date.is_a? Time
         start_date.to_time + GIGASECOND
     end      
 end
