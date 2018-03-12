79b3c68914c4d6656c0a41cdad220156bf21ab39
Teresa Ritorto <tritorto@gmail.com>
2018-03-12 16:48:03 +1100

    Add files via upload

---
+++ hamming/hamming.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+class Hamming
+    def self.compute(strand1, strand2)
+        strand1, strand2 = [strand1, strand2].sort_by(&:length)
+        pairs = strand1.chars.zip(strand2.chars)
+        pairs.count{ |item1, item2| item1 != item2 }
+    end
+end
